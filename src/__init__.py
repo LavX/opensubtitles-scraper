7c5e6d41161577e0e8b613a5b3c4f2ec348ebee3
Laszlo Toth <laszlo.toth@baxter-it.com>
2026-03-26 00:46:49 +0100

    chore: bump version to 1.2.1

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 """OpenSubtitles Web Scraper Service"""
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __author__ = "LavX"
 __description__ = "Web scraper service for OpenSubtitles.org using cloudscraper"
